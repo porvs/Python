a69ed652edcb3cbf58677b4acdc140ec05079b8a
Hayeong Jeong <hayoung00045@gmail.com>
2022-01-05 17:02:11 +0900

    update

--- class.py
+++ class.py
@@ -1,9 +1,29 @@
-class person:
-    def say_hello(self):
-        print("안녕!")
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
 
-    def say_hello(self):
-        print("안녕! 나는" + self.name)
+class SafeFourCal(FourCal):
+    def div(self):
+        if self.second == 0:
+            return 0
+        else:
+            return self.first / self.second
 
-p = person()
-p.say_hello()
+a = SafeFourCal(4, 0)
+print(a.div())
